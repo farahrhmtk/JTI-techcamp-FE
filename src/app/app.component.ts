8b4a3a213053b311ea330d2633942e2b28b35b26
farahrhmtk <farahrhmtk27@gmail.com>
2023-08-03 18:16:35 +0700

    Update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { TodoService } from './services/todo.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoService]
 })
 export class AppComponent {
   title = 'angular-to-do-list';
 }
+
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.services';
+import { Todo } from '../../todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
 
+export class TodosComponent implements OnInit{
+  todos: Todo[]=[];
+  name: any = '';
+  editMode: any[] = [];
+  
+  constructor(
+    private todoService: TodoService
+  ) {}
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe({
+      next: (todos) => {
+        console.log(todos);
+        this.todos = todos;
+
+        for (let i = 0; i < todos.length; i++) {
+          this.editMode.push(false);
+        }
+      },
+
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
